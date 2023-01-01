d1eae4cef6cc73c4ee17e8d38f2bd32bb083698a
fftcysstkm <t.satobm3012@gmail.com>
2023-01-01 19:18:13 +0900

    slidable導入

--- lib/pages/todo/todo_list.dart
+++ lib/pages/todo/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:roughly_scheduler/model/data/todo/todo.dart';
 
 // ignore: slash_for_doc_comments
@@ -35,14 +36,57 @@ class Todos extends StatelessWidget {
     return ListView.builder(
       itemCount: todos.length,
       itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            leading: const FlutterLogo(size: 56.0),
-            title: Text(todos[index].title),
-            trailing: const Icon(Icons.more_vert),
-          ),
-        );
+        return Slidable(
+            key: const ValueKey(0),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              dismissible: DismissiblePane(onDismissed: () {}),
+              dragDismissible: false,
+              children: [
+                // アーカイブボタン
+                SlidableAction(
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFF7BC043),
+                  foregroundColor: Colors.white,
+                  icon: Icons.archive,
+                ),
+                // 削除ボタン
+                SlidableAction(
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFFFE4A49),
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                ),
+                // 編集ボタン
+                SlidableAction(
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFF0392CF),
+                  foregroundColor: Colors.white,
+                  icon: Icons.edit,
+                ),
+              ],
+            ),
+            child: _TodoCard(todo: todos[index]));
       },
+    );
+  }
+}
+
+// ignore: slash_for_doc_comments
+/**
+ * Todoリスト1行のパーツ
+ *
+ */
+class _TodoCard extends StatelessWidget {
+  const _TodoCard({required this.todo});
+  final Todo todo;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const FlutterLogo(size: 56.0),
+        title: Text(todo.title),
+      ),
     );
   }
 }

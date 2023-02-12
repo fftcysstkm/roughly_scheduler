ac45542875999371264d2e2b0dabb0a24ea02e07
fftcysstkm <t.satobm3012@gmail.com>
2023-02-12 13:26:37 +0900

    settingページ少し追加

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -13,6 +13,42 @@ class SettingsPage extends StatelessWidget {
           centerTitle: true,
           title: const Text('Rough Scheduler'),
         ),
-        body: Center(child: const Text('settings')));
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text('プッシュ通知を許可する',
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  Expanded(child: Container()),
+                  const _Switch(),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _Switch extends StatefulWidget {
+  const _Switch();
+
+  @override
+  State<_Switch> createState() => _SwitchState();
+}
+
+class _SwitchState extends State<_Switch> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: light,
+        onChanged: ((value) {
+          setState(() {
+            light = value;
+          });
+        }));
   }
 }
